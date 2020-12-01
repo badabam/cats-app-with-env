6513611dccc2372da3e294825771ba4cf9f16cbd
Jeremias Erbs <jerry@badabam.de>
2020-12-01 12:14:38 +0100

    Move client serving from /api to server.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const axios = require("axios");
-const path = require("path");
 
 const app = express();
 const apiKey = process.env.API_KEY;
-
-// Serve any static files
-app.use(express.static(path.join(__dirname, "../client/build")));
-app.use(
-  "/storybook",
-  express.static(path.join(__dirname, "../client/storybook-static"))
-);
 
 app.get("/cats/random", (req, res) => {
   axios.get("https://api.thecatapi.com/v1/images/search").then((result) => {
@@ -30,12 +22,6 @@ app.get("/cats/breeds", (req, res) => {
       console.log(result.data.length);
       res.json(result.data);
     });
-});
-
-// Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require("./api/index");
 const port = process.env.PORT || 3001;
+const express = require("express");
+const path = require("path");
+
+// Handle React routing, return all requests to React app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Serve any static files
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
