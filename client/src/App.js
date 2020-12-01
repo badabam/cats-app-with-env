7f957c04d9c717fd59570edb236f99c703529f2d
Jeremias Erbs <jerry@badabam.de>
2020-12-01 12:17:49 +0100

    Add /api/cats/random and error output

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,24 @@ import "./App.css";
 
 function App() {
   const [catImage, setCatImage] = useState("");
+  const [error, setError] = useState();
 
   useEffect(() => {
+    fetch("/api/cats/random")
+      .then((res) => res.json())
+      .then((data) => setCatImage(data[0].url))
+      .catch(setError);
+
     fetch("/cats/random")
       .then((res) => res.json())
-      .then((data) => setCatImage(data[0].url));
+      .then((data) => setCatImage(data[0].url))
+      .catch(setError);
   }, []);
 
   return (
     <div className="App">
       <img src={catImage} alt="" />
+      {JSON.stringify(error)}
     </div>
   );
 }
