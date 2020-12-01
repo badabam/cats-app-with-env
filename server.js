2f8aca3851c3aaf8f4807761711fef99d223edc3
Jeremias Erbs <jerry@badabam.de>
2020-12-01 21:30:47 +0100

    Remove custom 404 page

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
-app.use('*', (err, req, res, next) => {
-  res.status(404).send(`Nothing found on path ${req.url}.`)
-})
-
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
 })
